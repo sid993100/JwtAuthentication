2dd6b6d4cf0557be8b5c829932f4c48924016cdb
Siddharth Pathak <sid993100@gmail.com>
2024-08-25 10:09:52 +0530

    intial commit

---
+++ src/test/java/com/sid993100/JwtAuthentication/JwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid993100.JwtAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
