95ad4f5ad63475eda2638f8e23fc3084979d9dd3
Siddharth Pathak <sid993100@gmail.com>
2024-08-26 14:26:03 +0530

    intial commit2

--- src/main/java/com/sid993100/JwtAuthentication/controllers/HomeController.java
+++ src/main/java/com/sid993100/JwtAuthentication/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.sid993100.JwtAuthentication.controllers;
 
+import com.sid993100.JwtAuthentication.model.User;
 import com.sid993100.JwtAuthentication.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/home")
@@ -14,8 +17,8 @@ public class HomeController {
     private UserService userService;
 
     @GetMapping("/users")
-    public String getUser(){
+    public List<User> getUser(){
         System.out.println("Getting users.");
-        return "Users";
+        return userService.getUsers();
     }
 }
